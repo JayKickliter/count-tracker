fc623226a1cf31d7653228520c8ae3f8b8200bb3
Jay Kickliter <jay@kickliter.com>
2022-05-03 15:23:19 -0500

    Initial commit

--- client/index.js
+++ client/index.js
@@ -1,91 +1,115 @@
-const {
-  Connection,
-  sendAndConfirmTransaction,
-  Keypair,
-  Transaction,
-  SystemProgram,
-  PublicKey,
-  TransactionInstruction,
-} = require("@solana/web3.js");
+// args:
+// 0: program ID
+// [1]: counter ID
+// [2]: auth pubKey
 
+const {
+    Connection,
+    sendAndConfirmTransaction,
+    Keypair,
+    Transaction,
+    SystemProgram,
+    PublicKey,
+    TransactionInstruction,
+} = require("@solana/web3.js");
+const Base58 = require('base58');
+const FS = require('fs');
 const BN = require("bn.js");
+const {readFile} = require('mz/fs');
 
 const main = async () => {
-  var args = process.argv.slice(2);
-  // args[0]: Program ID
-  // args[1] (Optional): Counter buffer account
-  const programId = new PublicKey(args[0]);
+    var args = process.argv.slice(2);
+    const programId = new PublicKey(args[0]);
 
-  console.log(programId.toBase58());
-  const connection = new Connection("https://api.devnet.solana.com/");
-  const feePayer = new Keypair();
+    const connection = new Connection("http://localhost:8899");
+    let feePayer = new Keypair();
 
-  console.log("Requesting Airdrop of 1 SOL...");
-  await connection.requestAirdrop(feePayer.publicKey, 2e9);
-  console.log("Airdrop received");
+    if (args.length > 2) {
+        let secretKeyString = await readFile(args[2], {encoding: 'utf8'});
+        console.log("Loaded Keypair from ", args[2]);
+        const secretKey = Uint8Array.from(Object.values(JSON.parse(secretKeyString)));
+        feePayer = Keypair.fromSecretKey(secretKey);
+    } else {
+        FS.writeFile(feePayer.publicKey.toBase58(), JSON.stringify(Uint8Array.from(feePayer.secretKey)), (err) => {
+            if (err)
+                console.log(err);
+            else {
+                console.log("File written successfully\n");
+            }
+        });
+    }
 
-  const counter = new Keypair();
-  let counterKey = counter.publicKey;
-  let tx = new Transaction();
-  let signers = [feePayer];
-  if (args.length > 1) {
-    console.log("Found counter address");
-    counterKey = new PublicKey(args[1]);
-  } else {
-    console.log("Generating new counter address");
-    let createIx = SystemProgram.createAccount({
-      fromPubkey: feePayer.publicKey,
-      newAccountPubkey: counterKey,
-      /** Amount of lamports to transfer to the created account */
-      lamports: await connection.getMinimumBalanceForRentExemption(8),
-      /** Amount of space in bytes to allocate to the created account */
-      space: 8,
-      /** Public key of the program to assign as the owner of the created account */
-      programId: programId,
+    console.log("feepayer pubKey:", feePayer.publicKey.toBase58());
+    console.log("Requesting Airdrop of 1 SOL...");
+    await connection.requestAirdrop(feePayer.publicKey, 2e9);
+    console.log("Airdrop received");
+
+    const counterAccount = new Keypair();
+    let counterKey = counterAccount.publicKey;
+    let tx = new Transaction();
+    let signers = [feePayer];
+    if (args.length > 1) {
+        console.log("Found address");
+        counterKey = new PublicKey(args[1]);
+    } else {
+        console.log("Generating new counter address");
+        let createIx = SystemProgram.createAccount({
+            fromPubkey: feePayer.publicKey,
+            newAccountPubkey: counterKey,
+            /** Amount of lamports to transfer to the created account */
+            lamports: await connection.getMinimumBalanceForRentExemption(36),
+            /** Amount of space in bytes to allocate to the created account */
+            space: 36,
+            /** Public key of the program to assign as the owner of the created account */
+            programId: programId,
+        });
+        signers.push(counterAccount);
+        tx.add(createIx);
+    }
+
+    let instr_dat = Buffer.from("");
+    // if (args.length > 2) {
+    //     let toIncrement = parseInt(args[2], 10);
+    //     console.log("toIncrement: ", toIncrement);
+    //     instr_dat = Buffer.from("    ");
+    //     instr_dat.writeInt32LE(toIncrement, 0);
+    //     console.log("Incrementing by: ", instr_dat.readUInt32LE());
+    // }
+
+    let counterIx = new TransactionInstruction({
+        keys: [
+            {
+                pubkey: counterKey,
+                isSigner: false,
+                isWritable: true,
+            },
+            {
+                pubkey: feePayer.publicKey,
+                isSigner: true,
+                isWritable: false,
+            },
+        ],
+        programId: programId,
+        data: instr_dat,
     });
-    signers.push(counter);
-    tx.add(createIx);
-  }
+    console.log("couinterIx:", counterIx);
+    tx.add(counterIx);
 
-  const idx = Buffer.from(new Uint8Array([0]));
-
-  let incrIx = new TransactionInstruction({
-    keys: [
-      {
-        pubkey: counterKey,
-        isSigner: false,
-        isWritable: true,
-      }
-    ],
-    programId: programId,
-    data: idx,
-  });
-  /*
-    TransactionInstruction({
-      keys: Array<AccountMeta>,
-      programId: PublicKey,
-      data: Buffer,
+    let txid = await sendAndConfirmTransaction(connection, tx, signers, {
+        skipPreflight: true,
+        preflightCommitment: "confirmed",
+        commitment: "confirmed",
     });
-  */
-  tx.add(incrIx);
 
-  let txid = await sendAndConfirmTransaction(connection, tx, signers, {
-    skipPreflight: true,
-    preflightCommitment: "confirmed",
-    confirmation: "confirmed",
-  });
-  console.log(`https://explorer.solana.com/tx/${txid}?cluster=devnet`);
-
-  data = (await connection.getAccountInfo(counterKey)).data;
-  count = new BN(data, "le");
-  console.log("Counter Key:", counterKey.toBase58());
-  console.log("Count: ", count.toNumber());
+    let data = (await connection.getAccountInfo(counterKey, "confirmed")).data;
+    console.log("New counter value: ", data.readUInt32LE(32));
+    console.log("Counter Key:", counterKey.toBase58());
 };
 
 main()
-  .then(() => {
-    console.log("Success");
-  })
-  .catch((e) => {
-    console.error(e);
-  });
+    .then(() => {
+        console.log("Success");
+    })
+    .catch((e) => {
+        console.error(e);
+    });
--- src/processor.rs
+++ src/processor.rs
@@ -1,16 +1,19 @@
+use base58::FromBase58;
 use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint::ProgramResult,
-    msg,
+    instruction::{AccountMeta, Instruction},
+    program::invoke_signed,
     program_error::ProgramError,
     pubkey::Pubkey,
+    system_instruction,
 };
 
 /// Define the type of state stored in accounts
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct AccountString {
-    pub memo: [u8; 6],
+pub struct CounterTracker {
+    pub count: u32,
 }
 
 /// Instruction processor
@@ -20,22 +23,61 @@ pub fn process_instruction(
     instruction_data: &[u8],
 ) -> ProgramResult {
     let accounts_iter = &mut accounts.iter();
-    let account = next_account_info(accounts_iter)?;
+    let global_counter = next_account_info(accounts_iter)?;
+    let user = next_account_info(accounts_iter)?;
+    let user_counter = next_account_info(accounts_iter)?;
 
-    if account.owner != program_id {
-        msg!("this program does not own account {:?}", account);
-        return Err(ProgramError::IncorrectProgramId);
-    }
+    assert!(user.is_signer);
 
-    let acct_memo = AccountString {
-        memo: {
-            let mut acct_str = [0; 6];
-            acct_str.copy_from_slice(&instruction_data[..6]);
-            acct_str
-        },
+    let auth_ctr_prog_id = {
+        let auth_ctr_prog = "Af4DodBScfdZ8Qyq7qf6sNFisJQopQ93adTawWvNxvFK";
+        let auth_ctr_prog = auth_ctr_prog.from_base58().unwrap();
+        let mut auth_ctr_prog_arr = [0_u8; 32];
+        auth_ctr_prog_arr.copy_from_slice(auth_ctr_prog.as_slice());
+        Pubkey::new_from_array(auth_ctr_prog_arr)
     };
 
-    acct_memo.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    let user_counter_seeds = &[user.key.as_ref(), global_counter.key.as_ref()];
+    let (user_counter_pda, bump) = Pubkey::find_program_address(user_counter_seeds, program_id);
+    assert_eq!(&user_counter_pda, user_counter.key);
 
+    match instruction_data.split_first() {
+        Some((&0, &[])) => {
+            let system_program_info = next_account_info(accounts_iter)?;
+            invoke_signed(
+                &system_instruction::create_account(
+                    user.key,
+                    user_counter.key,
+                    100000000,
+                    36,
+                    &auth_ctr_prog_id,
+                ),
+                &[
+                    user.clone(),
+                    user_counter.clone(),
+                    system_program_info.clone(),
+                ],
+                &[&[user.key.as_ref(), global_counter.key.as_ref(), &[bump]]],
+            )?
+        }
+        Some((&1, &[])) => {
+            invoke_signed(
+                &Instruction {
+                    program_id: auth_ctr_prog_id,
+                    accounts: vec![
+                        AccountMeta::new(*user_counter.key, false),
+                        AccountMeta::new(*user_counter.key, true),
+                    ],
+                    data: vec![],
+                },
+                &[user_counter.clone()],
+                &[&[user.key.as_ref(), global_counter.key.as_ref(), &[bump]]],
+            )?;
+            let mut tracker = CounterTracker::try_from_slice(&global_counter.data.borrow())?;
+            tracker.count += 1;
+            tracker.serialize(&mut &mut global_counter.data.borrow_mut()[..])?;
+        }
+        _ => return Err(ProgramError::InvalidInstructionData),
+    }
     Ok(())
 }
